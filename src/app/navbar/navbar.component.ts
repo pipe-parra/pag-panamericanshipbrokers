cd8f9c13c54c9b3f4afdc14b046f6922f0f30a01
pipe <felipe.parra@blackmind.cl>
2022-05-28 22:12:28 -0400

    cambio en componente de contacto

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,6 +13,15 @@ export class NavbarComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.playSound();
   }
 
+  playSound(){
+    console.log("funciona");
+    let audio = new Audio();
+    audio.src = '../assets/sound/fondopanamerican.mp3';
+    audio.load();
+    audio.play();
+  
+  }
 }
